ad400651d83dc272050da86102fcc3e49c1e2cbe
Corbin Crutchley <corbin.crutchley.contractor@hilton.com>
2020-01-20 16:32:06 -0800

    Initial work outlining article

---
+++ index.js
@@ -0,0 +1,18 @@
+// Initialize using signing secret from environment variables
+const { createEventAdapter } = require('@slack/events-api');
+const slackEvents = createEventAdapter(process.env.SLACK_SIGNING_SECRET);
+const port = process.env.PORT || 3000;
+
+// Attach listeners to events by Slack Event "type". See: https://api.slack.com/events/message.im
+slackEvents.on('message', (event) => {
+  console.log(`Received a message event: user ${event.user} in channel ${event.channel} says ${event.text}`);
+});
+
+// Handle errors (see `errorCodes` export)
+slackEvents.on('error', console.error);
+
+// Start a basic HTTP server
+slackEvents.start(port).then(() => {
+  // Listening on path '/slack/events' by default
+  console.log(`server listening on port ${port}`);
+});
